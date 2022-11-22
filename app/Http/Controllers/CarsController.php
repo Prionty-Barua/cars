dcc685170bf9526f786e1fc4111b471b11b16079
Prionty-Barua <priontycsebd@gmail.com>
2022-11-22 15:51:56 +0600

    first commit

---
+++ app/Http/Controllers/CarsController.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Car;
+
+class CarsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $cars = Car::all();
+
+        return view ('cars.index',[
+            'cars' => $cars
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('cars.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        // $cars = new Car;
+        // $cars->name = $request->input('name');
+        // $cars->founded = $request->input('founded');
+        // $cars->description = $request->input('description');
+        // $cars->save();
+
+        $cars = Car::create([
+            'name' => $request->input('name'),
+            'founded' => $request->input('founded'),
+            'description' => $request->input('description'),
+        ]);
+
+        return redirect('/cars');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $cars = Car::find($id)->first();
+        return view('cars.edit')->with('car',$cars);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $cars = Car::where('id',$id)->update([
+            'name' => $request->input('name'),
+            'founded' => $request->input('founded'),
+            'description' => $request->input('description'),
+        ]);
+        return redirect('/cars');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Car $car)
+    {
+        $car->delete();
+
+       return redirect('/cars');
+    }
+}
---
+++ resources/views/cars/create.blade.php
@@ -0,0 +1,36 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="m-auto w-4/8 py-24">
+   <div class="text-center">
+    <h1 class="text-5xl uppercase-bold">
+        Create Cars</h1>
+   </div>
+</div>
+<div class="flex justify-center pt-20">
+    <form action="/cars" method="POST">
+        @csrf
+        <div class="block">
+            <input type="text"
+            class="block shadow-5xl mb-10 p-2 w-80 italic
+            placeholder-gray-400"
+            name="name"
+            placeholder="Brand name..."><br><br>
+
+            <input type="text"
+            class="block shadow-5xl mb-10 p-2 w-80 italic
+            placeholder-gray-400"
+            name="founded"
+            placeholder="founded..."><br><br>
+
+            <input type="text"
+            class="block shadow-5xl mb-10 p-2 w-80 italic
+            placeholder-gray-400"
+            name="description"
+            placeholder="Description..."><br><br>
+
+            <button type="submit" class="bg-green-500 block shadow-5xl mb-10 p-2 w-80 uppercase font-bold">Submit</button>
+        </div>
+    </form>
+</div>
+@endsection
